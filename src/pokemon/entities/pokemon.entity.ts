60a03681e8ce6fb70b80b6264af12eb01768f75f
JhoanS5 <JhoanS5.Dev@gmail.com>
2026-02-26 00:28:26 -0500

    End Proyect-Pokedex

---
+++ src/pokemon/entities/pokemon.entity.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Pokemon extends Document {
+    // public id: string // Mongo me lo da
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    public name: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    public numberPokemon: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
